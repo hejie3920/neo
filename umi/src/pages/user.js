8123b5db889746c850ab5eba1855621918906a38
hejie <1484215077@qq.com>
2020-01-14 11:28:27 +0800

    umi引入工具

--- umi/src/pages/user.js
+++ umi/src/pages/user.js
@@ -1,9 +1,44 @@
 import styles from './user.css';
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import { connect } from 'react-redux';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page user</h1>
-    </div>
-  );
+const mapStateToProps = state => ({
+  user: state.user,
+});
+@connect(mapStateToProps)
+class user extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.dispatch({ type: 'user/asyncFetch', payload: 'nihao' });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          type="primary"
+          onClick={e => {
+            this.props.dispatch({
+              type: 'user/asyncFetch',
+              payload: '爸爸',
+            });
+          }}
+        >
+          点击
+        </Button>
+
+        {this.props.user.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+    );
+  }
 }
+
+export default user;
